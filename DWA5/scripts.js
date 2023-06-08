abe0b10a266d0db1586fa0b6ff080648813848f3
Thapelo-Molosiwa <0728921928p@gmail.com>
2023-06-08 16:24:05 +0200

    dwa 6

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -1,28 +1,29 @@
 const form = document.querySelector("[data-form]");
+
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
+
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
   const wholenumber = dividend / divider;
 
   if (isNaN(dividend) || isNaN(divider)) {
 
-    document.body.innerText = " Something critical went wrong. Please reload the page.";
+    document.body.innerText =
+      " Something critical went wrong. Please reload the page.";
 
     console.error("Invalid input provided. Program crashed");
-
-  } else if (dividend == "" || divider === "") {
-
-    result.innerText = " Division not performed. Both values are required in inputs. Try again ";
-
-  } else if (dividend < 0 || divider < 0) {
-
-    result.innerText = "Division not performed. Invalid number provided. Try again";
     
+  } else if (dividend == "" || divider === "") {
+    result.innerText =
+      " Division not performed. Both values are required in inputs. Try again ";
+    console.error("lol ae spane sahn.");
+  } else if (dividend < 0 || divider < 0) {
+    result.innerText =
+      "Division not performed. Invalid number provided. Try again";
+
+    console.error("cant run");
   } else result.innerText = Math.floor(wholenumber);
 });
-
-
